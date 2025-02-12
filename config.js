252e222f8d8442a9e8074f1ce22a00a3290e84e0
trenunn <trenunnally@gmail.com>
2025-02-12 12:57:10 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://asset.cloudinary.com/dnerpkyvy/db085dfc653997ad0123702b20789a63", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dnerpkyvy/video/upload/v1739382019/SpotifyMate.com_-_Just_the_Two_of_Us__feat._Bill_Withers__-_Grover_Washington__Jr._rdtmvz.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
