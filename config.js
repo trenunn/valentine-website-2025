c3092bbbf921aff2e62cd00cc1c799c942f82c97
trenunn <trenunnally@gmail.com>
2025-02-12 13:06:42 -0500

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Beautiful Valentine? 💝",
+    pageTitle: "???",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
